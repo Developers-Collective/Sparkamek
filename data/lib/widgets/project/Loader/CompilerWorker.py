9eba4129616784a2cf1575b4ab5692603dfd2718
Synel <montagna.samuel@gmail.com>
2023-08-19 19:25:16 +0200

    loader compilation

--- data/lib/widgets/project/Loader/CompilerWorker.py
+++ data/lib/widgets/project/Loader/CompilerWorker.py
@@ -2,7 +2,7 @@
 
     # Libraries
 from PySide6.QtCore import Signal, QThread
-import os
+import os, subprocess, sys, shutil
 
 from data.lib.qtUtils import QBaseApplication, QUtilsColor
 from ..LogType import LogType
@@ -26,6 +26,7 @@ class CompilerWorker(QThread):
         super(CompilerWorker, self).__init__()
 
         self._data = data
+        self._output_file = self._data.get('outputFile', None)
 
         path = os.path.abspath(data['path']).replace('\\', '/')
 
@@ -42,9 +43,63 @@ class CompilerWorker(QThread):
     def run(self) -> None:
         self.log_info_all('Starting compilation...', False)
 
-        # todo: compile
 
+        command = [f'{self._cwd}/powerpc-eabi-as.exe', '-mregnames', self._project_full_path, '-o', f'{self._cwd}/{self._project_name}.o']
+
+        try:
+            self.log_info_all('Assembling...', False)
+            self.log_info(f'Executing command: {command}', False)
+            p = subprocess.Popen(command, stdout = subprocess.PIPE)
+            output = p.communicate()[0].decode('utf-8')
+            error_val = p.poll()
+
+        except Exception as e:
+            self.log_error(f'Error while compiling: {e}')
+            self.error.emit(str(e))
+            return
+        
+        if error_val != 0:
+            self.log_error(f'Error while compiling: {output}')
+            self.error.emit(output)
+            return
+
+
+        command = [f'{self._cwd}/powerpc-eabi-ld.exe', '-Ttext', '0x800046F0', '--oformat', 'binary', f'{self._cwd}/{self._project_name}.o', '-o', f'{self._cwd}/{self._project_name}.bin'] # todo: add address to settings
+
+        try:
+            self.log_info_all('Linking...', False)
+            self.log_info(f'Executing command: {command}', False)
+            p = subprocess.Popen(command, stdout = subprocess.PIPE)
+            output = p.communicate()[0].decode('utf-8')
+            error_val = p.poll()
+
+        except Exception as e:
+            self.log_error(f'Error while linking: {e}')
+            self.error.emit(str(e))
+            return
+        
+        if error_val != 0:
+            self.log_error(f'Error while linking: {output}')
+            self.error.emit(output)
+            return
+
+
+        try:
+            os.remove(f'{self._cwd}/{self._project_name}.o')
+
+        except Exception as e:
+            self.log_warning(f'Error while deleting .o file: {e}')
+
+        if self._output_file:
+            try:
+                shutil.copyfile(f'{self._cwd}/{self._project_name}.bin', self._output_file)
+
+            except Exception as e:
+                self.log_warning(f'Error while copying output file: {e}')
+
+        self.log_success('Compilation completed successfully!')
         self.done.emit()
+
 
     def log_info(self, msg: str, invisible: bool = False) -> None:
         msg = msg.strip()
