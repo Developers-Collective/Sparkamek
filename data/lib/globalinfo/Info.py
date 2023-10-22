86e57051d25d0a319fdf4f0eb8e9bf955a67bf66
Synel <70210528+Synell@users.noreply.github.com>
2023-10-22 10:45:13 +0200

    version change

--- data/lib/globalinfo/Info.py
+++ data/lib/globalinfo/Info.py
@@ -10,7 +10,7 @@ class Info:
     def __new__(cls) -> None:
         return None
 
-    build: str = '07e7d494'
+    build: str = '07e7d4b4'
     version: str = 'Experimental'
 
     application_name: str = 'Sparkamek'
