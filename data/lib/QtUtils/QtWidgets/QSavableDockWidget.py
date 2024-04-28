a68ba0594c7aabca4bdedcde5a404f3e2b5675bf
Synel <70210528+Synell@users.noreply.github.com>
2024-04-28 09:00:56 +0200

    lib update

--- data/lib/QtUtils/QtWidgets/QSavableDockWidget.py
+++ data/lib/QtUtils/QtWidgets/QSavableDockWidget.py
@@ -1,7 +1,7 @@
 #----------------------------------------------------------------------
 
     # Libraries
-from PySide6.QtWidgets import QDockWidget, QMainWindow
+from PySide6.QtWidgets import QDockWidget, QMainWindow, QWidget
 from PySide6.QtCore import Qt
 #----------------------------------------------------------------------
 
@@ -11,21 +11,26 @@ class QSavableDockWidget(QDockWidget):
     #     super().__init__(*args, **kwargs)
     #     self._name = name
 
+
     # @property
     # def name(self) -> str:
     #     return self._name
+
 
     @property
     def area(self) -> Qt.DockWidgetArea:
         return self.parent().dockWidgetArea(self)
 
+
     @property
     def is_floating(self) -> bool:
         return self.isFloating()
 
+
     @property
     def tabified(self) -> list:
         return self.parent().tabifiedDockWidgets(self)
+
 
     def to_dict(self) -> dict:
         return {
@@ -37,23 +42,23 @@ class QSavableDockWidget(QDockWidget):
             # 'orientation': self.orientation().value
         }
 
+
     def load_dict(self, main_window: QMainWindow, data: dict, orientation: Qt.Orientation = Qt.Orientation.Vertical) -> None:
         if data['isFloating']:
             main_window.addDockWidget(Qt.DockWidgetArea(1), self, orientation)
             self.setFloating(True)
             self.move(data['area'][0], data['area'][1])
-            self.resize(data['size'][0], data['size'][1])
 
         else:
             main_window.addDockWidget(Qt.DockWidgetArea(data['area']), self, orientation)
-
-        # self.resize(data['size'][0], data['size'][1])
 
         for w in data['tabified']:
             if type(w) == str: w = main_window.findChild(QDockWidget, w, Qt.FindChildOption.FindDirectChildrenOnly)
             if w: main_window.tabifyDockWidget(self, w)
 
+        main_window.resizeDocks([self], [data['size'][0 if orientation == Qt.Orientation.Vertical else 1]], orientation)
         self.setVisible(data['visible'])
+
 
     @staticmethod
     def from_dict(main_window: QMainWindow, data: dict, orientation: Qt.Orientation = Qt.Orientation.Vertical) -> 'QSavableDockWidget':
