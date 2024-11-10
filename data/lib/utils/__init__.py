08ab00af1d5e54c878865701713c361db6c8f7c3
Synel <70210528+Synell@users.noreply.github.com>
2024-11-10 15:13:57 +0100

    xml update

--- data/lib/utils/__init__.py
+++ data/lib/utils/__init__.py
@@ -3,7 +3,7 @@
     # Libraries
 from .classproperty import classproperty
 from .deprecated import deprecated
-from .Singleton import Singleton
+from .Singleton import Singleton, singleton
 from .StaticClass import StaticClass
 from .Color import Color
 from .AbstractFactory import AbstractFactory
